bc0fbe1f0add65b0fb8f9d32e82985e835dd987c
sonamnahar21 <sonamnahar21@gmail.com>
2019-03-02 15:30:12 -0800

    added more tests

--- test/testGetWeather.js
+++ test/testGetWeather.js
@@ -8,12 +8,26 @@ describe('Get Current Weather', () => {
         expect(response.statusCode).to.equal(200);
       });
     });
-    it('answer with weather response coord object', () => {
+    it('answer with weather response `coord` object', () => {
        getWeather.currentWeather('San Jose', function(response) {
         let body = JSON.parse(response.body);
         expect(body.coord.lon).to.exist;
         expect(body.coord.lat).to.exist;
+        expect(body.coord.lon).to.be.a('number');
+        expect(body.coord.lat).to.be.a('number');
       });
     });
+    it('answer with weather response `base` object', () => {
+      getWeather.currentWeather('San Jose', function(response) {
+       let base = JSON.parse(response.body);
+        expect(base.base).to.exist;
+        expect(base.base).to.be.a('string');
+        expect(base.base).to.be.equals('stations');
+     });
+   });
+  it('answer with weather response `main` object', () => {
+  });
+  it('answer with weather response `wind` object', () => {
+  });
   });
 });
