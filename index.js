6dc088ac596ce4e8cc97fd2f0a8c601f8cdb6067
sonamnahar21 <sonamnahar21@gmail.com>
2019-03-02 12:42:09 -0800

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var getWeather = require('./src/getWeather');
+var readline = require('readline-sync');
+var cityname = readline.question("Enter City Name: ");
+
+getWeather.currentWeather(cityname, function(err, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(body); 
+  }
+});
---
+++ src/getWeather.js
@@ -0,0 +1,21 @@
+let request = require('request');
+let apiKey = '43810163e3221fb614d03d65c5a88b15';
+
+var currentWeather = function(city,callback) {
+    request.get(
+        {
+            url: "http://api.openweathermap.org/data/2.5/weather", 
+            qs: {
+                    q: city, 
+                    apiKey : apiKey
+                }
+        },
+        function(error, response, body){
+            if (error || response.statusCode !== 200) {
+                return callback(error || {statusCode: response.statusCode});
+              }
+            callback(null, JSON.parse(body));  
+        });
+}
+
+module.exports ={ currentWeather };
